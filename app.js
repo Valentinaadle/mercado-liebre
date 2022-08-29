0794b6b4fddbf7ac4c6ca1407982eeb40774c3a3
Valentinaadle <valentinaadle1@gmail.com>
2022-08-29 20:25:41 -0300

    modifique ruta

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require ('path');
 app.use(express.static('public'));
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, '/home.html'))
 });
 
