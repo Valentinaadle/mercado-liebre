712c77149a56251b6559fd1434b30a4b3c841928
Valentinaadle <valentinaadle1@gmail.com>
2022-08-29 20:22:57 -0300

    modifique

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const path = require ('path');
 app.use(express.static('public'));
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/home.html'))
 });
 
 app.get('/register', (req, res) => {
